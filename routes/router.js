dffe6d8eb7e216dea7c57101139712d0f9ce86d5
owarewaa <akosuowarewaa360@gmail.com>
2024-07-01 16:34:09 +0000

    added endpoints

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { getEvent, postEvent } from "../controllers/controller";
 
 // Creating a router app
 const eventRouter = Router()
 
-eventRouter.post('/events', );
-eventRouter.get('/events', );
+eventRouter.post('/events',postEvent );
+eventRouter.get('/events', getEvent);
 eventRouter.get('/events/:id', );
 eventRouter.patch('/events/:id', );
 eventRouter.delete('/events/:id', );
