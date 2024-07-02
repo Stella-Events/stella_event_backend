7165c0d1860b5192515ae46bde6e8d8855dfac67
Camey Fiifi Danchi <capochinolive100@gmail.com>
2024-07-02 09:13:20 +0000

    api documentation

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,7 @@
-
-
-
-
-
+import { EventModel } from "../model/event.js"
 
 // Creating a get event
-export const getEvent = async (req,res) => {
+export const getEvent = async (req,res,next) => {
 
 try {
       const getEvents = await EventModel.find()
@@ -22,5 +18,44 @@ export const postEvent = async (req,res) => {
         res.json(postEvents)
     } catch (error) {
         
+    }
+}
+
+// creating get by Id
+
+export const getEventId = async (req,res,next) => {
+  try {
+      const getEventsId = await EventModel.findById(req.params.id);
+      res.json(getEventId)
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+// pacthing an event
+
+export const patchEvent = async (req, res,next) => {
+    try {
+ 
+     // update recipe by Id
+     const updateEvent = await EventModel.findByIdAndUpdate(req.params.id,req.body)
+     // return response
+      res.json(updateEvent)
+    } catch (error) {
+     next(error)
+    }
+ }
+
+ // deleting an event
+
+ export const deleteEvent = async (req, res, next) => {
+    try {
+        // delete recipe by id
+        const deletedEvents = await EventModel.findByIdAndDelete(req.params.id);
+        // return response
+        res.json(deletedEvents)
+    } catch (error) {
+        next(error)
     }
 }
