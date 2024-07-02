2b15650d6269f0a5efeace06ba7671bcf9c26aad
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-07-02 09:16:53 +0000

    Created Category Model

--- model/event.js
+++ model/event.js
@@ -1,9 +1,10 @@
 import { Schema, model } from "mongoose";
-
+import {toJSON} from "@reis/mongoose-to-json"
 //Creating a Schema
 
 const eventSchema = new Schema({
     name:{type:String, required:true},
+    categoryId:{type: Types.ObjectId, ref: 'Category', required: true},
     date:{type:Date, required:true},
     price:{type:String, required:true},
     location:{type:String, required:true},
@@ -11,5 +12,7 @@ const eventSchema = new Schema({
 },{
     timestamps:true
 });
+
+eventSchema.plugin(toJSON);
 
 export const EventModel = model('event', eventSchema)
