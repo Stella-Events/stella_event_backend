4702f0fdaf0b9b00dbffe29873b0c9900fb72756
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-07-02 12:50:40 +0000

    Editing Category Model

--- model/category.js
+++ model/category.js
@@ -4,9 +4,10 @@ import {toJSON} from "@reis/mongoose-to-json";
 const categorySchema = new Schema({
     eventType:{type:String, enum:['wedding', 'conference'], required:true},
     description:{type:String},
-    image:{type:String},
+    image:{type:String,required:true},
+    
 });
 
 categorySchema.plugin(toJSON);
 
- export const CategoryModel = model('Category', 'categorySchema')
+ export const CategoryModel = model('Category', categorySchema)
--- model/event.js
+++ model/event.js
@@ -1,12 +1,15 @@
 import { Schema, model } from "mongoose";
 import {toJSON} from "@reis/mongoose-to-json"
-//Creating a Schema
 
+
+//Creating a Schema
 const eventSchema = new Schema({
     name:{type:String, required:true},
     categoryId:{type: Types.ObjectId, ref: 'Category', required: true},
     date:{type:Date, required:true},
-    price:{type:String, required:true},
+    time:{type: String},
+    price:{type:Number, required:true},
+    tickets: [{ type: String }],
     location:{type:String, required:true},
     flierUrl:{type:String, required:true}
 },{
