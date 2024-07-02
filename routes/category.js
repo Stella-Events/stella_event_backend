49d4884cddb28d51c8ae57b45efd5c8d57bd7145
owarewaa <akosuowarewaa360@gmail.com>
2024-07-02 16:03:25 +0000

    added category routes

---
+++ routes/category.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+
+const categoryRouter = Router();
+
+categoryRouter.post('/categories', );
+categoryRouter.get('/categories', );
+categoryRouter.get('/categories/:id', );
+categoryRouter.patch('/categories/:id',  );
+categoryRouter.delete('/categories/:id', );
+
+
+export default categoryRouter;
