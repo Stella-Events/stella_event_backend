9db0ed443d70a89c8b608ef6cf0945fed8c78012
encrentsil <nyamekye.nesther.esther599@gmail.com>
2024-07-03 10:34:42 +0000

    New model update

--- model/category.js
+++ model/category.js
@@ -2,7 +2,7 @@ import {Schema, model } from "mongoose";
 import {toJSON} from "@reis/mongoose-to-json";
 
 const categorySchema = new Schema({
-    eventType:{type:String, enum:['wedding', 'conference']},
+    eventType:{type:String, enum:['wedding', 'conference', 'concert']},
     description:{type:String},
     image:{type:String},
     
